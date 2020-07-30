364629ff258dab6016dfd3e1fd503158a0f599e1
Paula Alvarez <paula.alzq4@gmail.com>
2020-07-30 09:45:34 -0500

    first commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 export const getGifs = async( category ) => {
 
     // const urlR = 'https://api.giphy.com/v1/gifs/trending&api_key=vQ7cVmYpXEKP0FWzkuGqA7T3g4htHqVZ';
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category )}&limit=5&api_key=vQ7cVmYpXEKP0FWzkuGqA7T3g4htHqVZ`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category )}&limit=30&api_key=vQ7cVmYpXEKP0FWzkuGqA7T3g4htHqVZ`;
     const response = await fetch( url );
     const { data } = await response.json();
     const gifs = data.map( img => {
