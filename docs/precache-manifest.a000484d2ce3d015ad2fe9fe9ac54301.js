1ed4f2d7a373fc9bde7f232402f596909be230d7
Paula Alvarez <paula.alzq4@gmail.com>
2020-07-30 09:47:32 -0500

    first commit

---
+++ docs/precache-manifest.a000484d2ce3d015ad2fe9fe9ac54301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef5a3023a18adf3f32e1b01a5ca2208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c2b192b631d0b0dc573",
+    "url": "/static/css/main.257c1e78.chunk.css"
+  },
+  {
+    "revision": "9b2b836cf3b6de39ef80",
+    "url": "/static/js/2.32bf6af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32bf6af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2b192b631d0b0dc573",
+    "url": "/static/js/main.88697604.chunk.js"
+  },
+  {
+    "revision": "e1f2535515280bbae9bc",
+    "url": "/static/js/runtime-main.532f7a3e.js"
+  }
+]);
